9cd6d89c9d2cfb784d8306a0f0842a833fc391f5
jeanduong <jean.duong@gmail.com>
2016-12-02 11:54:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jeanduong/ocrapplication/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.jeanduong.ocrapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends Activity {
+
+    Bitmap img; //our image
+    TessBaseAPI tess_engine; //Tess API reference
+    String datapath = ""; //path to folder containing language data file
+    String lang = "fra";
+
+    final String TAG = "OCR over RGB";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView vw = (ImageView) findViewById(R.id.display_view_name);
+
+        img = BitmapFactory.decodeResource(getResources(), R.drawable.dooblink_crop);
+
+        vw.setImageBitmap(img);
+
+        // This part works fine!
+        // Disable it to focus on OpenCV stuffs (in another activity).
+
+        tess_engine = new TessBaseAPI();
+        datapath = getFilesDir() + "/tesseract/";
+
+        //make sure training data has been copied
+        checkFile(new File(datapath + "tessdata/"));
+
+        tess_engine.init(datapath, lang);
+        tess_engine.setImage(img);
+
+        String txt = tess_engine.getUTF8Text();
+
+        tess_engine.end();
+
+        // Print text in IDE output console
+        Log.i(TAG, txt);
+
+    }
+
+    private void copyFiles() {
+        try {
+            //location we want the file to be at
+            String filepath = datapath + "/tessdata/" + lang + ".traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/" + lang + ".traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkFile(File dir) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/" + lang + ".traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+}
---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jeanduong/Implementation/tess/tess-two/bin/AndroidManifest.xml : \
+/Users/jeanduong/Implementation/tess/tess-two/AndroidManifest.xml \
